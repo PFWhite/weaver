32f1c8bec93ab8f78f424ea422ab25dc0d9396ec
Patrick White <pfwhite9@gmail.com>
2018-07-02 09:35:48 -0400

    update render_include.js with comment describing usage

--- src/middleware/render_include.js
+++ src/middleware/render_include.js
@@ -1,3 +1,9 @@
+/*
+  This middleware takes an object and includes that into any subsequent render calls
+
+  This is often used for including deployment information into the html, like the
+  host of a static resource
+ */
 module.exports = function (options={}) {
     return async function (ctx, next) {
         var render = ctx.render
