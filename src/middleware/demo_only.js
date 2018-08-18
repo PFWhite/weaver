0101be37ef0554d6fe6262406191d453ffcdf5f2
Patrick White <pfwhite9@gmail.com>
2018-08-18 17:38:27 -0400

    make demo_only not dependent on the available routes

--- src/middleware/demo_only.js
+++ src/middleware/demo_only.js
@@ -1,13 +1,20 @@
 module.exports = function (password) {
     return async function (ctx, next) {
-        var pass = ( ctx.request.body  || {} ).demopass
-        if (pass == password) {
-            ctx.session.demo_authorized = true
+        var pass = ( ctx.request.body  || {} ).demopass,
+            wantedPage = ( ctx.request.body || {} ).wantedPage || ctx.request.url
+        if ( ctx.request.url === '/DEMOBLOCKPAGE' )  {
+            if (pass == password) {
+                ctx.session.demo_authorized = true
+                ctx.redirect(wantedPage)
+            }
         }
         if (ctx.session.demo_authorized) {
             await next()
         } else {
-            ctx.body = '<form action="/" method="POST"><input name="demopass" type="password" placeholder="Password" value=""/><button type="submit">Submit</button></form>'
+            ctx.body = `<form action="/DEMOBLOCKPAGE" method="POST">` +
+                `<input name="demopass" type="password" placeholder="Password" value=""/>` +
+                `<input name="wantedPage" type="text" style="display:none;" value="${wantedPage}"/>` +
+                `<button type="submit">Submit</button></form>`
         }
     }
 }
