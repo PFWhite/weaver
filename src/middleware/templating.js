240f4269509adbf2853cee725d56ddf0a155c4c9
Patrick White <pfwhite9@gmail.com>
2018-09-04 12:44:12 -0400

    enhace templating so that globals can be added to the templater

--- src/middleware/templating.js
+++ src/middleware/templating.js
@@ -1,12 +1,15 @@
 var nunjucks = require('nunjucks'),
     views = require('koa-views')
 
-module.exports = function (templateDir) {
+module.exports = function (templateDir, globals={}) {
     // need to make a loader for extends
     // https://github.com/queckezz/koa-views/issues/104
     var nunjucksEnv = new nunjucks.Environment(
         new nunjucks.FileSystemLoader(templateDir)
     )
+    Object.keys(globals).forEach(key => {
+        nunjucksEnv.addGlobal(key, globals[key])
+    })
     return views(templateDir, {
         options: {
             nunjucksEnv
