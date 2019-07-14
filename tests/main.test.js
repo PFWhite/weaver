be8252acbf20ef7177546a68f972855181a2b3d3
Patrick White <pfwhite9@gmail.com>
2019-07-14 12:20:17 -0400

    change to jest for testing #3

--- tests/main.test.js
+++ tests/main.test.js
@@ -1,6 +1,3 @@
-global.chai = require('chai')
-global.sinon = require('sinon')
-
 const index = require('../index.js')
 
 describe('middleware', () => {
---
+++ tests/middleware/demo_only.js
@@ -0,0 +1,20 @@
+var demoOnly = require('../../src/middleware/demo_only.js')
+
+describe('demo only', () => {
+    beforeEach(() => {
+    })
+    it('should return a function', () => {
+    })
+    describe('ctx.request.url === /DEMOBLOCKPAGE', () => {
+        beforeEach(() => {
+        })
+        describe('checkPass resolves', () => {
+            beforeEach(() => {
+            })
+            it('should call ctx.redirect', () => {
+            })
+        })
+        describe('checkPass rejects', () => {
+        })
+    })
+})
